ad834b04ec8df54be9907a9f24025311e5e7c0fc
nadavmarkuss <74564127+nadavmarkuss@users.noreply.github.com>
2022-11-15 23:39:42 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,8 @@
 scp -i "id_rsa" /var/lib/jenkins/workspace/Dev8200/docker-compose-prod.yaml \
 ec2-user@172.31.24.68:/home/ec2-user/
 
+ssh ec2-user@test docker-compose up
+ssh ec2-user@test docker compose down
+ssh ec2-user@test sleep 15
+ssh ec2-user@test docker rmi(name of image)
+ssh ec2-user@test system docker prune --volumes
