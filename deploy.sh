5218eff463980b53988de35d097e639a14471091
nadavmarkuss <74564127+nadavmarkuss@users.noreply.github.com>
2022-11-15 03:39:10 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp -i "id_rsa" /var/lib/jenkins/workspace/Dev8200/docker-compose-prod.yaml \
+scp -i "/home/ec2-user/.ssh/id_rsa" /var/lib/jenkins/workspace/Dev8200/docker-compose-prod.yaml \
 ec2-user@172.31.24.68:/home/ec2-user/
 
