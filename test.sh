4a6aeffc10922fda6ce5746da3cef47702e76bc0
nadavmarkuss <74564127+nadavmarkuss@users.noreply.github.com>
2022-11-18 15:23:56 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+
+                HTTP_STATUS=`curl -o /dev/null -s -w "%{http_code}\n" http://localhost:5000/` 
+		               if [ $HTTP_STATUS -eq 200 ];
+		               then
+		      		        echo "TEST: SUCCES"
+		               else
+				            echo "TEST: FAIL"
+				            exit 1
+		               fi
+                docker-compose down  
