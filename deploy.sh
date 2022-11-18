8f641da20fefac979bfdc5af75f8ae12cb61b9e1
nadavmarkuss <74564127+nadavmarkuss@users.noreply.github.com>
2022-11-18 14:27:11 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ scp -i ~/.ssh/id_rsa /var/lib/jenkins/workspace/Dev8200/docker-compose-prod.yaml
 ec2-user@172.31.24.68:/home/ec2-user/
 
 scp -i ~/.ssh/id_rsa /var/lib/jenkins/workspace/Dev8200/docker-compose-prod.yaml \
-ec2-user@prod/home/ec2-user/
+ec2-user@prod:/home/ec2-user/
 
 
 docker-compose rm -f
