bf25bf7d6250e7f41c9c4d9f211c961b6af81339
nadavmarkuss <74564127+nadavmarkuss@users.noreply.github.com>
2022-11-20 10:32:12 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,14 @@ echo "deploying to ${MachineName} machine..."
 scp -i ~/.ssh/id_rsa /var/lib/jenkins/workspace/Dev8200/docker-compose-prod.yaml \
 ec2-user@${MachineName}:/home/ec2-user/
 
+# connecting to the input machine and running multiple commands:
 ssh  -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no ec2-user@${MachineName} << EOF
   docker-compose -f docker-compose-prod.yaml down
   docker-compose -f docker-compose-prod.yaml up -d
   sleep 10
 EOF
-  
+
+# if deploying to test move test scrypt to test machine and run it:
 if [ $MachineName==test ]; then
   	scp -i ~/.ssh/id_rsa /var/lib/jenkins/workspace/Dev8200/test.sh \
 	ec2-user@test:/home/ec2-user/
